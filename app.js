6fff79ce436e936522e769d0054715a314d3a73c
srinivas17acc <srinivas17acc@gmail.com>
2022-06-27 00:20:55 +0530

    added routes in app js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const config = require('./src/config');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
+const routes = require('./src/routes/');
 
 
 const app = express();
@@ -10,5 +11,7 @@ app.use(helmet());
 app.use(express.json());  
 app.use(express.urlencoded({ extended: true }));  
 app.use(mongoSanitize());
+
+app.use('/map', routes);
 
 module.exports = app;
