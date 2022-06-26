7d3be8e81197fb9cac02e2619d382f9fc0817f08
srinivas17acc <srinivas17acc@gmail.com>
2022-06-26 23:04:33 +0530

    user model initial commit

---
+++ src/model/user.model.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 40
+    },
+    userName: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 40
+    },
+    email: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 40
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 200
+    },
+    createAt: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
